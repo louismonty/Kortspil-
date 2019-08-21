e0648df5e0cad77e70513409b206322d3bf4bd63
louismonty <louis.mail.dk@gmail.com>
2019-08-21 19:29:48 +0200

    valg af kort
    kortet kan nu skifte men der mangler 1 pokemon

--- kortspil/Kortspil_pokemon.py
+++ kortspil/Kortspil_pokemon.py
@@ -8,22 +8,24 @@ black = 0, 0, 0
 
 random_pokemon = random.randint(1,4)
 if random_pokemon == 1:
-    print("pica") 
+    pokemon = pygame.image.load("/Users/louis/Documents/GitHub/Kortspil-/kortspil/pica.png")
 if random_pokemon == 2:
+    pokemon = pygame.image.load("/Users/louis/Documents/GitHub/Kortspil-/kortspil/bulba.png")
     print ("bulba")
 if random_pokemon == 3:
+    pokemon = pygame.image.load("/Users/louis/Documents/GitHub/Kortspil-/kortspil/char.png")
     print ("char")
 if random_pokemon == 4:
     print ("squrt")
         
 screen = pygame.display.set_mode(size)
 
-pokemon = pygame.image.load("\Users\mathi\OneDrive\Skrivebord\niggachu.png")
+
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     
     screen.fill(black)
-    screen.blit(pokemon)
+    screen.blit(pokemon,(1,1))
     pygame.display.flip()
